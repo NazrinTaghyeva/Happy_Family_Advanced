28115d76c624c04aaf24bd4b83a6c8339b8cfae1
Nazryn <80715402+NazrinTaghyeva@users.noreply.github.com>
2022-07-31 12:24:22 +0400

    Update Pet.java

--- happyFamily/src/Pet.java
+++ happyFamily/src/Pet.java
@@ -1,11 +1,52 @@
 import java.util.Arrays;
 
 public class Pet {
-    String species;
-    String nickName;
-    int age;
-    int trickLevel;
-    String[] habits;
+    private String species;
+    private String nickName;
+    private int age;
+    private int trickLevel;
+    private String[] habits;
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getTrickLevel() {
+        return trickLevel;
+    }
+
+    public void setTrickLevel(int trickLevel) {
+        this.trickLevel = trickLevel;
+    }
+
+    public String[] getHabits() {
+        return habits;
+    }
+
+    public void setHabits(String[] habits) {
+        this.habits = habits;
+    }
+
 
     public String toString() {
         return species + "{" +
