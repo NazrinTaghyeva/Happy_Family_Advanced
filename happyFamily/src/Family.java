de7d14387ebae960eade4274707a07a0c4b0cdf7
Nazryn <80715402+NazrinTaghyeva@users.noreply.github.com>
2022-07-31 12:22:51 +0400

    Update Family.java

--- happyFamily/src/Family.java
+++ happyFamily/src/Family.java
@@ -1,32 +1,18 @@
 import java.util.Arrays;
-
 public class Family {
     private Human mother;
     private Human father;
     private Human[] children;
     private Pet pet;
 
-    public Family() {
-    }
 
-    public Family(Human mother,Human father,Human[] children,Pet pet){
-        this.mother = mother;
-        this.father = father;
-        this.children = children;
-        this.pet = pet;
-
-    }
 
     public Family(Human mother,Human father){
-
-    }
-
-    public String toString() {
-        return "Family{" +
-                "mother=" + mother.getName() + " " + mother.getSurname() +
-                ", father=" + father.getName() + " " + father.getSurname() +
-                ", children=" + Arrays.deepToString(children) +
-                ", pet=" + pet + '}';
+        this.mother = mother;
+        this.father = father;
+        this.children = new Human[0];
+        father.setFamily(this);
+        mother.setFamily(this);
     }
 
 
@@ -63,6 +49,54 @@ public class Family {
     }
 
 
+    public String toString() {
+        return "Family{" +
+                "mother=" + mother.getName() + " " + mother.getSurname() +
+                ", father=" + father.getName() + " " + father.getSurname() +
+                ", children=" + Arrays.deepToString(children) +
+                ", pet=" + pet +
+                '}';
+    }
+
+
+    public void addChild(Human child) {
+        if (child != null) {
+            Human[] children = new Human[this.children.length + 1];
+            for (int i = 0; i < this.children.length; i++) {
+                children[i] = getChildren()[i];
+            }
+            children[children.length - 1] = child;
+            setChildren(children);
+            child.setFamily(this);
+        } else {
+            getChildren();
+        }
+
+    }
+    public void deleteChild(int in) {
+        if (in > getChildren().length - 1) {
+            getChildren();
+        } else {
+            Human[] children = new Human[getChildren().length - 1];
+            if (children.length == 0) {
+                children = new Human[0];
+            } else {
+                for (int i = 0, j = 0; i < getChildren().length; i++) {
+                    if (i == in) {
+                        continue;
+                    }
+                    children[j++] = getChildren()[i];
+                }
+            }
+            setChildren(children);
+        }
+
+    }
+
+    public int countFamily() {
+        return 2 + this.children.length;
+
+    }
 
 
 }
