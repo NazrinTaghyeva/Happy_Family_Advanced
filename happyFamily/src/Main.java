528d1568eb7fe4b6070753d90b30a2d2d091d5c3
Nazryn <80715402+NazrinTaghyeva@users.noreply.github.com>
2022-08-08 01:03:33 +0400

    Update Main.java

--- happyFamily/src/Main.java
+++ happyFamily/src/Main.java
@@ -1,17 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        Pet pet = new Pet("Female","Koukie",2,15,new String[]{"eating","sleeping"});
+        Human mother = new Human("Nuray","Doe",1985);
+        Human father = new Human("John","Doe",1980);
 
-        Pet pet = new Pet("cat", "Kuki", 5, 25, new String[]{"eating", "sleeping"});
-
-        Human mother = new Human("Melina", "Doe", 1983);
-        Human father = new Human("John", "Doe", 1975);
         Family mFamily = new Family(mother,father);
-        Human child1 = new Human("Nazryn","Doe",2003,90,pet,father,mother, new String[][]{{"thursday", "watch the film"}});
-        Human child2 = new Human("Kate","Doe",2020,80,pet,father,mother, new String[][]{{"saturday", "meet the friends"}});
+        Human child = new Human("Zuzu","Doe",2000,80,new String[][]{{"monday","watch the film"},{"sunday","meet the friends"}});
+        Human child1 = new Human("Kate", "Parry", 2004, 90, new String[][]{{"monday","watch the film"},{"sunday","meet the friends"}});
 
+
+
+        mFamily.addChild(child);
         mFamily.addChild(child1);
-        mFamily.addChild(child2);
         System.out.println(mFamily);
+        mFamily.feed();
+        mFamily.deleteChild(1);
+        System.out.println(mFamily);
+        System.out.println(mFamily.countFamily());
     }
-
 }
